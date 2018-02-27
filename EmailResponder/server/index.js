83f31b3fe398d612e22749280ea7cebb17d826a1
Patrick Conde <patrick.conde.costas@gmail.com>
2018-02-26 16:52:01 -0800

    At 22 for now

--- EmailResponder/server/index.js
+++ EmailResponder/server/index.js
@@ -1,10 +1,10 @@
 // import express from 'express'; This syntax will be used for frontend not server side node
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy; // because we just need the strategy module in this case
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({hi: 'there'});
-});
+passport.use(new GoogleStrategy());
 
 app.listen(5000);
