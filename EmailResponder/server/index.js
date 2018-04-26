ae32993d78a02e556fb2132abef4bfc1d9b55526
Patrick Conde <patrick.conde.costas@gmail.com>
2018-04-25 17:00:36 -0700

    adding mongoDB

--- EmailResponder/server/index.js
+++ EmailResponder/server/index.js
@@ -1,11 +1,16 @@
 // import express from 'express'; This syntax will be used for frontend not server side node
 const express = require('express');
-require('./services/passport') // because we're not exporting anything from passport.js you can write it like this
+const mongoose = require('mongoose');
+const keys = require('./config/keys')
+require('./services/passport'); // because we're not exporting anything from passport.js you can write it like this
+
+mongoose.connect(keys.mongoURI);
+// we want to commit our mongo info to github
 
 const app = express();
 
 require('./routes/authRoutes.js')(app); // we require the function and then immediately invokes the app function
-// doesn't have to be written like this, its simply refactoring. 
+// doesn't have to be written like this, its simply refactoring.
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
